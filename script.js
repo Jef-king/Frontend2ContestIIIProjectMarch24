26fd07e5a109acbc9b432a0dc412c2921b1c591f
jefking <jefking@outlook.in>
2024-03-30 23:05:12 +0530

    alldone

--- script.js
+++ script.js
@@ -5,39 +5,45 @@ let emailError = document.querySelector('#erEmail');
 let passwordError = document.querySelector('#erPassword');
 let signupForm = document.querySelector('#signUp');
 let successMessage=document.querySelector('#success');
-let submitBtn=document.querySelector("#checkSubmit")
 
 emailInput.addEventListener("blur", () => {
     const validate = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
     const email = emailInput.value;
     if (!validate.test(email)) {
       erEmail.textContent = "Make sure email is more than 3 characters and has @ and a .";
-      submitBtn.disabled = true;
-      successMessage.textContent=''
+      successMessage.textContent='';
     } else {
       erEmail.textContent = "";
-      emailContainer.style.borderColor = "";
-      submitBtn.disabled = false;
+      successMessage.textContent='';
     }
   });
 passwordInput.addEventListener("blur", () => {
     let passwordValue=passwordInput.value;
     if (passwordValue.length < 8) {
         passwordError.textContent = 'Make sure password is more than 8 characters.';
-        submitBtn.disabled = true;
-        successMessage.textContent=''
+        successMessage.textContent='';
     }else{
         passwordError.textContent = '';
-        submitBtn.disabled = false;
+        successMessage.textContent='';
     }
   });
 
 
 signupForm.addEventListener('submit', (e) => {
     e.preventDefault();
+    let emailValue = emailInput.value.trim();
+    if (!emailValue.match(/^[\w-.]+@[\w-]+(\.[\w-]+)*$/)) {
+        emailError.textContent = 'Make sure email is more than 3 characters and has @ and a .';
+        return;
+    } 
+    let passwordValue = passwordInput.value.trim();
+    if (passwordValue.length < 8) {
+        passwordError.textContent = 'Make sure password is more than 8 characters.';
+        return;
+    } 
     emailError.textContent = '';
     passwordError.textContent = '';
-    successMessage.textContent='All good to go'
+    successMessage.textContent='All good to go';
     emailInput.value = '';
     passwordInput.value = '';
 });
